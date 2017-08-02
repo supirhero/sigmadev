88fda8d0d0df2881e7c72d2fdb08ad6da6734bae
dyaskur <dyaskur@gmail.com>
2017-08-03 06:28:35 +0700

    Fix session token

--- application/controllers/Project.php
+++ application/controllers/Project.php
@@ -7,7 +7,52 @@ class Project extends CI_Controller
         parent::__construct();
         $this->load->model('M_project');
         $this->load->model('M_business');
+        $this->load->model('M_session');
 
+        //TOKEN LOGIN CHECKER
+        if(isset($_GET['token'])){
+            $datauser["data"] = $this->M_session->GetDataUser($_GET['token']);
+
+            $decoded_user_data =$datauser;
+            //    print_r($decoded_user_data);
+            $this->datajson['token'] = $_GET['token'];
+
+        }
+        elseif(isset($_SERVER['HTTP_TOKEN'])){
+            $datauser["data"] = $this->M_session->GetDataUser($_SERVER['HTTP_TOKEN']);
+
+            $decoded_user_data = $datauser["data"];
+            $this->datajson['token'] = $_SERVER['HTTP_TOKEN'];
+        }
+        else{
+            $error['error']="Login First!";
+            echo json_encode($error);
+            die();
+        }
+        //if login success
+        if(!isset($decoded_user_data[0])){
+            //get user data from token
+
+            //for login bypass ,this algorithm is not used
+            //$this->datajson['userdata'] = (array)$decoded_user_data['data'];
+            //this code below for login bypass
+            $this->datajson['userdata'] = $decoded_user_data;
+        }
+        //if login fail
+        else {
+            echo $decoded_user_data[0];
+            die();
+        }
+
+        if($datauser["data"]["SESSION_EXPIRED"] <= time())
+        {
+            $error['error']="session is expired";
+            echo json_encode($error);
+            die();
+        }
+        else{
+            $this->M_session->update_session($this->datajson['token']);
+        }
     }
 
     function index(){
--- application/controllers/Report.php
+++ application/controllers/Report.php
@@ -10,15 +10,23 @@ class Report extends CI_Controller {
         $this->load->model('M_timesheet');
         $this->load->model('M_report');
         $this->load->model('M_business');
+        $this->load->model('M_session');
+
 
         //TOKEN LOGIN CHECKER
-        if(isset($_SERVER['HTTP_TOKEN'])){
-            $decoded_user_data =(array) $this->token->decodetoken($_SERVER['HTTP_TOKEN']);
-            $this->datajson['token'] = $_SERVER['HTTP_TOKEN'];
-        }
-        elseif(isset($_GET['token'])){
-            $decoded_user_data =(array) $this->token->decodetoken($_GET['token']);
+        if(isset($_GET['token'])){
+            $datauser["data"] = $this->M_session->GetDataUser($_GET['token']);
+
+            $decoded_user_data =$datauser;
+            //    print_r($decoded_user_data);
             $this->datajson['token'] = $_GET['token'];
+
+        }
+        elseif(isset($_SERVER['HTTP_TOKEN'])){
+            $datauser["data"] = $this->M_session->GetDataUser($_SERVER['HTTP_TOKEN']);
+
+            $decoded_user_data = $datauser["data"];
+            $this->datajson['token'] = $_SERVER['HTTP_TOKEN'];
         }
         else{
             $error['error']="Login First!";
@@ -28,6 +36,7 @@ class Report extends CI_Controller {
         //if login success
         if(!isset($decoded_user_data[0])){
             //get user data from token
+
             //for login bypass ,this algorithm is not used
             //$this->datajson['userdata'] = (array)$decoded_user_data['data'];
             //this code below for login bypass
@@ -37,6 +46,16 @@ class Report extends CI_Controller {
         else {
             echo $decoded_user_data[0];
             die();
+        }
+
+        if($datauser["data"]["SESSION_EXPIRED"] <= time())
+        {
+            $error['error']="session is expired";
+            echo json_encode($error);
+            die();
+        }
+        else{
+            $this->M_session->update_session($this->datajson['token']);
         }
 
     }
