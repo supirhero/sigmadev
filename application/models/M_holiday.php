784357ffa6c91889aa32737bae29a3cce819124c
geryruslandi <geryruslandi@gmail.com>
2017-08-07 23:21:43 +0700

    dataset in progress

---
+++ application/models/M_holiday.php
@@ -0,0 +1,64 @@
+<?php
+
+Class M_holiday extends CI_Model{
+    function selectHoliday(){
+        $query = $this->db->get('P_HOLIDAY');
+        $hasil = $query->result_array();
+        return $hasil;
+
+    }
+    /*  function editMenu($data){
+    $this->db->where('id', $id);
+    $this->db->update('mytable', $data);
+  }*/
+
+
+    function getMaxHoliday(){
+        return $this->db->query("select nvl(max(holiday_id)+1, 700001) as NEW_ID from p_holiday")->row()->NEW_ID;
+    }
+
+    public function insertHoliday($data){
+        //$this->db->insert("P_HOLIDAY", $data);
+        $sql="INSERT INTO P_HOLIDAY (HOLIDAY_ID,HOLIDAY,HOLIDAY_START,HOLIDAY_END, COLOR) VALUES (
+  '".$data['HOLIDAY_ID']."',
+  '".$data['HOLIDAY']."',
+  ".$data['HOLIDAY_START'].",
+  ".$data['HOLIDAY_END'].",
+  '".$data['COLOR']."')";
+        $q=$this->db->query($sql);
+    }
+
+    function lastLogin($data){
+        $delivDate = date('d-m-Y');
+        $sql="UPDATE P_HOLIDAY SET HOLIDAY_DATE=to_date('".$delivDate."','dd-mm-yy') WHERE HOLIDAY_ID='".$user_id."'";
+        $q = $this->db->query($sql);
+    }
+
+    public function deleteHoliday($id){
+        $this->db->delete('P_HOLIDAY', array('HOLIDAY_ID' => $id));
+    }
+
+    function editHoliday($data){
+        $sql="UPDATE P_HOLIDAY SET HOLIDAY='".$data['HOLIDAY']."', HOLIDAY_START=to_date('".$data['HOLIDAY_START']."','yyyy-mm-dd'),HOLIDAY_END=to_date('".$data['HOLIDAY_END']."','yyyy-mm-dd') WHERE HOLIDAY_ID='".$data['HOLIDAY_ID']."'";
+        $q = $this->db->query($sql);
+    }
+
+    function selectCalendar(){
+        $sql="select HOLIDAY_ID, HOLIDAY, TO_CHAR(HOLIDAY_START,'yyyy-mm-dd') AS HOLIDAY_START,TO_CHAR(HOLIDAY_END,'yyyy-mm-dd') AS HOLIDAY_END, COLOR FROM P_HOLIDAY where HOLIDAY_START is not null";
+        $query = $this->db->query($sql);
+        $hasil = $query->result_array();
+        return $hasil;
+    }
+
+    public function deleteCalendar($id){
+        $this->db->delete('P_HOLIDAY', array('HOLIDAY_ID' => $id));
+    }
+
+    public function view_row(){
+        return $this->db->get('P_HOLIDAY')->result();
+    }
+}
+
+
+
+?>
