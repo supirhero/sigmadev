13b86a6df4bb52aa6035405a4fc0b87af5f8d63e
dyaskur <dyaskur@gmail.com>
2017-08-12 18:28:20 +0700

    count total pv  (not complete)

--- application/controllers/Chart.php
+++ application/controllers/Chart.php
@@ -74,8 +74,8 @@ SELECT  SUM (ev) AS ev, a.project_id, project_status
     }
     function test_pv()
     {
-        $start = "01-DEC-16";
-        $end = "20-DEC-16";
+        $start = "2016-12-01";
+        $end = "2016-12-30";
         $query = $this->db->query("
 select sum(pv) as pv, project_id, min(start_date) as aza, max(start_date) as anu, max(finish_date) as anjay from (
              select ( select current_duration * CASE
@@ -115,6 +115,43 @@ select sum(pv) as pv, project_id, min(start_date) as aza, max(start_date) as anu
                   WHERE b.project_id=8538862 AND CONNECT_BY_ISLEAF = 1
              CONNECT BY b.wbs_parent_id = PRIOR b.wbs_id
              START WITH b.wbs_parent_id IS NULL) group by project_id 
+");
+        print_r($query->result());
+
+    }
+    function test_pv2()
+    {
+        $project_id = "8538862";
+        $start = "2016-12-01";
+        $end = "2016-12-30";
+        $query = $this->db->query("
+WITH date_range AS (
+    SELECT  ACTUAL_START_DATE as start_date
+           ,ACTUAL_END_DATE as end_date
+    FROM    PROJECTS where project_id='$project_id'
+    )
+SELECT  t2.\"Week\",t2.\"startdate\",t2.\"enddate\",
+(SELECT COUNT (b1.dt)
+               FROM  v_holiday_excl_weekend b1
+              WHERE b1.dt BETWEEN t2.\"startdate\" AND t2.\"enddate\" 
+             )*(SELECT CASE
+    WHEN (
+      sum(RESOURCE_WBS) > 0
+      AND sum(RESOURCE_WBS) IS NOT NULL
+    ) THEN
+      sum(RESOURCE_WBS)
+    ELSE
+      1
+      END from wbs  WHERE project_id='$project_id'
+      GROUP BY project_id 
+     )
+             
+            FROM   (SELECT  LEVEL \"Week\"
+       ,TRUNC(start_date + (7 * (LEVEL - 1)),'IW') \"startdate\"
+       ,TRUNC(start_date + (7 * (LEVEL - 1)),'IW') + 6 \"enddate\"
+       ,TO_CHAR(start_date + (7 * (LEVEL - 1)),'IW') \"Iso Week\"
+FROM   date_range t2
+CONNECT BY LEVEL <= (TRUNC(end_date,'IW') - TRUNC(start_date,'IW')) / 7 + 1) t2
 ");
         print_r($query->result());
 
@@ -163,7 +200,7 @@ SELECT * from resource_pool WHERE project_id='8532760' AND   ROWNUM <= 99
 ");
        // print_r($query->result());
         $query = $this->db->query("
-SELECT * from wbs_pool WHERE  ROWNUM <= 99
+SELECT * from v_holiday_excl_weekend WHERE  ROWNUM <= 99
      
 ");
         //print_r($query->result());
@@ -173,15 +210,23 @@ SELECT * from detail_capture WHERE  project_id='8538862' AND ROWNUM <= 99
 ");
      //  print_r($query->result());
         $query = $this->db->query("
-SELECT * from capture_wbs WHERE  project_id='8538862'  AND  ROWNUM <= 99
-     
-");
-     //   print_r($query->result());
-        $query = $this->db->query("
-SELECT * from tb_pv_project WHERE  ROWNUM <= 99
+SELECT * from wbs WHERE  project_id='7778226'  AND  ROWNUM <= 99
      
 ");
         print_r($query->result());
+        $query = $this->db->query("
+SELECT * from projects WHERE ROWNUM <= 99 AND ACTUAL_START_DATE BETWEEN TO_DATE('2016-09-09','YYYY-MM-DD') AND TO_DATE('2016-12-09','YYYY-MM-DD')
+     
+");
+        print_r($query->result());
+        $query = $this->db->query("
+
+SELECT    COUNT (b1.dt) n_holiday_today
+               FROM wbs a1, v_holiday_excl_weekend b1
+              WHERE a1.project_id='8538862' AND b1.dt BETWEEN a1.start_date AND a1.finish_date
+           GROUP BY a1.wbs_id     
+");
+      //  print_r($query->result());
         $query = $this->db->query("
 select * from all_source where name = 'DAILY_UPDATE';
      
@@ -231,6 +276,7 @@ SELECT  LEVEL \"Week\"
 FROM   date_range t2
 CONNECT BY LEVEL <= (TRUNC(tanggal,'IW') - TRUNC(tanggal,'IW')) / 7 + 1
 ");
+
         //  print_r($query->result());
 $project_id = "8538862";
         $query = $this->db->query("
@@ -240,15 +286,16 @@ WITH date_range AS (
     FROM    PROJECTS where project_id='$project_id'
     )
 SELECT  t2.\"Week\",t2.\"startdate\",t2.\"enddate\",
-            (select sum(t1.pv) pv from tb_rekap_project t1 where project_id='$project_id' and t1.tanggal between t2.\"startdate\" and t2.\"enddate\" ) as pv,
             (select sum(t1.ev) ev from tb_rekap_project t1 where project_id='$project_id' and t1.tanggal between t2.\"startdate\" and t2.\"enddate\" ) as ev,
             (select sum(t1.ev)/sum(t1.pv) spi from tb_rekap_project t1 where project_id='$project_id' and t1.tanggal between t2.\"startdate\" and t2.\"enddate\" ) as spi
 FROM   (SELECT  LEVEL \"Week\"
        ,TRUNC(start_date + (7 * (LEVEL - 1)),'IW') \"startdate\"
-       ,TRUNC(start_date + (7 * (LEVEL - 1)),'IW') + 6 \"enddate\"
+       ,TRUNC(start_date + (7 * (LEVEL - 1)),'IW') + 4 \"enddate\"
        ,TO_CHAR(start_date + (7 * (LEVEL - 1)),'IW') \"Iso Week\"
 FROM   date_range t2
 CONNECT BY LEVEL <= (TRUNC(end_date,'IW') - TRUNC(start_date,'IW')) / 7 + 1) t2
+
+
 ");
           print_r($query->result());
     }
