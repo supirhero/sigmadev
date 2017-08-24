f81a45ccb71fb4b8a96551ef9e3b9fa4a8f4b76b
geryruslandi <geryruslandi@gmail.com>
2017-08-24 20:06:26 +0700

    add project member and add test function

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -22,4 +22,8 @@ class Welcome extends CI_Controller {
 	{
 		$this->load->view('welcome_message');
 	}
+	public function test(){
+	    echo "file get content<br>";
+	    print_r($_FILES);
+    }
 }
