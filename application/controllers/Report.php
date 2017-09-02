9e8f27f7a1f8fcd4151c173c85b620ce2eb468dc
dyaskur <dyaskur@gmail.com>
2017-09-02 13:39:23 +0700

    Fix Data

--- application/controllers/Report.php
+++ application/controllers/Report.php
@@ -1217,7 +1217,7 @@ class Report extends CI_Controller {
         $result["r_monthly"] = $query->result();
         $known = array();
         $knownz = array();
-        $filtered = array_filter($result["r_monthly"], function ($val) use (&$known,&$knownz) {
+        $filtered["r_monthly"] = array_filter($result["r_monthly"], function ($val) use (&$known,&$knownz) {
             $unique = !in_array($val->BU_ALIAS, $knownz);
             $known[] = $val;
             $knownz[] = $val->BU_ALIAS;
