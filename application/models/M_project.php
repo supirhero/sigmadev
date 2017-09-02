1ea25172af70c5e8a93c7f62bf79100c366923ae
dyaskur <dyaskur@gmail.com>
2017-09-02 12:08:34 +0700

    Fix Data

--- application/models/M_project.php
+++ application/models/M_project.php
@@ -303,7 +303,7 @@ class M_project extends CI_Model {
              FROM USERS a INNER JOIN projects b ON a.user_id = b.created_by
              INNER JOIN p_bu z on b.bu_code = z.bu_code
              INNER JOIN p_project_category d on b.TYPE_OF_EFFORT=d.ID
-                  )) ";
+                  ) ";
 
           $sql.=" order by date_created desc";
         return $this->db->query($sql)->result_array();
