2d9b139a9134826a316018873ded8a51f38a2277
dyaskur <dyaskur@gmail.com>
2017-09-02 19:00:15 +0700

    email send to geryruslan

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -360,7 +360,7 @@ class Home extends CI_Controller {
         $config['upload_path']		= 'asset/user/';
         $config['allowed_types']	= 'jpg|png|gif|jpeg';
         $config['overwrite'] = TRUE;
-        $config['max_size']			= 5000000;
+        $config['max_size']			= 7000000;
         $config['file_name'] = $this->datajson['userdata']['USER_ID'].".".$extension;
 
         $this->load->library('upload', $config);
