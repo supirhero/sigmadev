ad12ab4395c3eb4d6c072df3b8c4eb0f3517caa7
dyaskur <dyaskur@gmail.com>
2017-09-03 09:40:41 +0700

    Data user

--- application/models/M_timesheet.php
+++ application/models/M_timesheet.php
@@ -427,10 +427,10 @@ GROUP BY TS_DATE")->result_array();
             $SUBMITDATE = $data['SUBMIT_DATE'];
 
 
-        $this->db->query("UPDATE TEMPORARY_TIMESHEET 
+
+        $this->db->query("update timesheet
                              set TS_ID = '$TS_ID' , SUBJECT='$SUBJECT', MESSAGE = '$MESSAGE', HOUR_TOTAL = '$HOUR_TOTAL', TS_DATE ='$TS_DATE', WP_ID='$WP_ID', LATITUDE='$LATITUDE', LONGITUDE='$LONGITUDE',SUBMIT_DATE = to_timestamp('$SUBMITDATE','YYYY-MM-DD HH24:MI:SS') 
-                           where TS_ID = '$TS_ID'
-                             ");
+                           where TS_ID = '$TS_ID'");
     }
 
     function checkTSData($wp,$date){
@@ -596,8 +596,7 @@ GROUP BY TS_DATE")->result_array();
 
             $this->db->query("UPDATE TEMPORARY_TIMESHEET 
                              set TS_ID = '$TS_ID' , SUBJECT='$SUBJECT', MESSAGE = '$MESSAGE', HOUR_TOTAL = '$HOUR_TOTAL', TS_DATE ='$TS_DATE', WP_ID='$WP_ID', LATITUDE='$LATITUDE', LONGITUDE='$LONGITUDE',IS_VALID=1,SUBMIT_DATE = to_timestamp('$SUBMITDATE','YYYY-MM-DD HH24:MI:SS'),ACTION = 'edit',RH_ID='$rh_id' 
-                           where TS_ID = '$TS_ID'
-                             ");
+                           where TS_ID = '$TS_ID'");
 
     }
 
