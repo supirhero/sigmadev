bae7ba54867e155ebedf09a1dc0307f337090cf3
dyaskur <dyaskur@gmail.com>
2017-09-08 14:02:55 +0700

    Fix token

--- application/models/M_session.php
+++ application/models/M_session.php
@@ -1,18 +1,42 @@
 <?php
 Class M_session extends CI_Model{
-    function GetDataUser($token){
-        //return $this->db->get_where($table,$where);
-        $result=array();
-        $sql="select * from USER_SESSION WHERE SESSION_TOKEN='$token'";
+    function GetDataUser(){
+        $token = (isset($_SERVER['HTTP_TOKEN']))?$_SERVER['HTTP_TOKEN']:(isset($_GET['token'])?$_GET['token']:false);
+        $result["token"]= $token;
+
+        if(!$token)
+        {
+            $result["error"]= "Token required(must login first)";
+            $result["status"]= 401;
+        }
+        else{
+            $result=array();
 
         $sql="SELECT * FROM USER_SESSION INNER JOIN USERS ON USER_SESSION.USER_ID = USERS.USER_ID WHERE USER_SESSION.SESSION_TOKEN='$token'";
-      //  $sql="SELECT column_name FROM user_tab_cols WHERE table_name = 'USER_SESSION' ";
         $q = $this->db->query($sql);
 
         if($q->num_rows() > 0){
-            $result = $q->row_array();
+            $result["token"] = $token;
+            if($result["data"]["SESSION_EXPIRED"] <= time())
+            {
+                $result["error"]= "Your token is expired";
+                $result["status"]= 401;
+
+            }
+            else{
+                $this->update_session($token);
+                $result["status"]= 200;
+                $result["data"] = $q->row_array();
+            }
         }
-        return $result;
+        else{
+            $result["status"]= 401;
+            $result["error"]= "Your token is invalid or expired";
+        }
+        }
+
+
+            return $result;
     }
 
     public function insert_session($user_id){
@@ -20,12 +44,12 @@ $new_session = md5(time().rand(1,999999));
         $this->db->set('SESSION_ID',1);
         $this->db->set('SESSION_TOKEN',$new_session);
         $this->db->set('USER_ID',$user_id);
-        $this->db->set('SESSION_EXPIRED',time()+7*24*60*60);
+        $this->db->set('SESSION_EXPIRED',time()+30*60);
         $this->db->insert("USER_SESSION");
 return $new_session;
     }
     public function update_session($token){
-        $newexpiry=time()+7*24*60*60;
+        $newexpiry=time()+30*60;
         $sql="update USER_SESSION set SESSION_EXPIRED='".$newexpiry."' where SESSION_TOKEN='".$token."'";
         $q = $this->db->query($sql);
     }
