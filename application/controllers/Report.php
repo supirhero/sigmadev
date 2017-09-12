217de90277909efae2b29fd50b6751c498d817a5
Faishol Tri Afandi <faishol.afandi@sigma.co.id>
2017-09-13 02:27:39 +0700

    restore data

--- application/controllers/Report.php
+++ application/controllers/Report.php
@@ -1369,7 +1369,7 @@ class Report extends CI_Controller {
         {
             $object->$key = $value;
         }
-        $filteredz["r_monthly"] = array($object);
+        $filtered["r_monthly"] = array($object);
         echo json_encode($filtered);
     }
             //report yearly overview
