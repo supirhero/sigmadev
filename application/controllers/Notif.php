4e8e2bd9063c3a7be6001875ca26dd0c114746a2
dyaskur <dyaskur@gmail.com>
2017-09-29 11:28:41 +0700

    Add index in notif controller

--- application/controllers/Notif.php
+++ application/controllers/Notif.php
@@ -61,6 +61,15 @@ class Notif extends CI_Controller
 
     }
 
+	    public function index(){
+	    $user_id = $this->datajson['userdata']["USER_ID"];
+	    $time = $this->input->post("time");
+	    $list_notif = $this->M_notif->getNotif($user_id,$time);
+	    $c['notif_list']=$list_notif["list"];
+	    $c['notif_info']=$list_notif["info"];
+	    echo json_encode($c,JSON_NUMERIC_CHECK);
+		$this->M_notif->setNotif($user_id);
+	    }
 	    public function get(){
 	    $user_id = $this->datajson['userdata']["USER_ID"];
 	    $time = $this->input->post("time");
